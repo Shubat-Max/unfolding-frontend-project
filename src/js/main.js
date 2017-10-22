f5a1a1d6ba981e2b888c724589f670446e18715e
Shubat Max <shubat.max@gmail.com>
2017-10-22 23:33:27 +0300

    Initial setup

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// With this file we can change the order of loading scripts
+
+/*
+ * Third party JS
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+
+/*
+ * Custom JS
+ */
+//= particles/common.js
